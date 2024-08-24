e69d3db7590f1a9ca0ea0f6b88a89c04fb606c61
BoundlessVoyager <zandel32@gmail.com>
2024-08-24 18:26:36 -0300

    feat: Página Preencher Seu Perfil

--- src/views/profile/profile.js
+++ src/views/profile/profile.js
@@ -1,13 +1,87 @@
 import * as React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Modal, FlatList } from "react-native";
 import { Avatar, IconButton, TextInput } from 'react-native-paper';
 import { styles } from "./style_profile";
 import Button from "../../components/button";
 
 export function telaPerfil({navigation}) {
+    const [selectedValue, setSelectedValue] = React.useState("Selecione uma opção");
+    const [modalVisible, setModalVisible] = React.useState(false);
+  
+    const options = [
+      { label: "Feminino", value: "F" },
+      { label: "Masculino", value: "M" },
+    ];
+  
+    const handleSelect = (item) => {
+      setSelectedValue(item.label);
+      setModalVisible(false);
+    };
     return (
-        <View>
-            
+        <View style={styles.container}>
+            <View style={styles.avatar_container}>
+                <Avatar.Image size={170} source={require('../../assets/avatar.png')}/>
+                <TouchableOpacity style={styles.iconButton}>
+                    <IconButton icon={"pencil"} style={styles.icon}/>
+                </TouchableOpacity>
+            </View>
+        
+        
+            <View>
+                <Text style={styles.text_input}>CPF</Text>
+                <TextInput 
+
+                    placeholder="Digite seu CPF" 
+                    style={styles.inputs} 
+                />
+                <Text style={styles.text_input}>Data de Nascimento</Text>
+                <TextInput 
+                    placeholder="00/00/0000" 
+                    style={styles.inputs} 
+                    left={<TextInput.Icon icon="calendar-month-outline"/>}
+                />
+                <View>
+                    <Text style={styles.text}>Gênero:</Text>
+                    <TouchableOpacity
+                        style={styles.selectButton}
+                        onPress={() => setModalVisible(true)}
+                    >
+                        <Text style={styles.selectText}>{selectedValue}</Text>
+                    </TouchableOpacity>
+
+                    <Modal
+                        transparent={true}
+                        animationType="slide"
+                        visible={modalVisible}
+                        onRequestClose={() => setModalVisible(false)}
+                    >
+                        <View style={styles.modalContainer}>
+                        <View style={styles.modalContent}>
+                            <FlatList
+                            data={options}
+                            keyExtractor={(item) => item.value}
+                            renderItem={({ item }) => (
+                                <TouchableOpacity
+                                style={styles.option}
+                                onPress={() => handleSelect(item)}
+                                >
+                                <Text style={styles.optionText}>{item.label}</Text>
+                                </TouchableOpacity>
+                            )}
+                            />
+                        </View>
+                        </View>
+                    </Modal>
+                </View>
+                <View style={styles.container_button}>
+                    <Button
+                        title={"Cadastrar"}
+                        onPress={() => {                           
+                        }}
+                    />
+                </View>
+            </View>
+        
         </View>
         
     );
--- src/views/profile/style_profile.js
+++ src/views/profile/style_profile.js
@@ -1,4 +1,77 @@
 import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        marginRight: 'auto',
+        marginLeft: 'auto',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    avatar_container:{
+        position: 'relative',
+    },
+    iconButton: {
+        position: 'absolute',
+        bottom: 0,
+        right: 0,
+        backgroundColor: 'rgb(211, 211, 211)',
+        borderRadius: 20,
+        padding: 2,
+    },
+    icon: {
+        margin: '0', 
+    },
+    inputs: {
+        height: 46,
+        width: 350,
+        backgroundColor: "rgb(228, 241, 238)",
+        borderRadius: 5,
+        marginVertical: 10,
+    },
+    text_input: {
+        margin: 5,
+    },
+    label: {
+        fontSize: 18,
+        marginBottom: 10,
+    },
     
+    text: {
+        fontSize: 14,
+        marginBottom: 10,
+    },
+    selectButton: {
+        backgroundColor: "rgb(228, 241, 238)",
+        padding: 10,
+        borderRadius: 5,
+        height: 46,
+        width: 350,
+        alignItems: 'center',
+    },
+    selectText: {
+        fontSize: 16,
+    },
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    modalContent: {
+        backgroundColor: 'white',
+        borderRadius: 10,
+        padding: 20,
+        width: 250,
+    },
+    option: {
+        padding: 10,
+    },
+    optionText: {
+        fontSize: 18,
+    },
+    container_button:{
+        marginTop: 30,
+    },
+
 });
