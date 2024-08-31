6ef0aec55b08609040b76b2f699572a76c28117d
Emanuel <emanuelcicero40@gmail.com>
2024-08-31 14:22:14 -0300

    feat: Criação de um banco de dados para médicos

---
+++ src/data/doctorsData.js
@@ -0,0 +1,29 @@
+const doctorsData = [
+    {
+      source: require('../assets/doctors/doctor1.png'),
+      title: 'Dr. Pedro Santos',
+      specialty: 'Cardiologista',
+      clinic: 'Clínica Vida Plena',
+    },
+    {
+      source: require('../assets/doctors/doctor2.png'),
+      title: 'Dra. Fernanda Rodrigues',
+      specialty: 'Dermatologista',
+      clinic: 'Clínica Pele Saudável',
+    },
+    {
+      source: require('../assets/doctors/doctor3.png'),
+      title: 'Dra. Luiza Souza',
+      specialty: 'Psicóloga',
+      clinic: 'Clínica VitaPlus',
+    },
+    {
+      source: require('../assets/doctors/doctor4.png'),
+      title: 'Dr. Carlos Pereira',
+      specialty: 'Clínico Geral',
+      clinic: 'Clínica Saúde e Bem-Estar',
+    },
+  ];
+  
+  export default doctorsData;
+  
